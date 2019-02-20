7772eb59129bf0ec2e24aa5ad7cf1be93db8ad43
joancolina819 <37549304+joancolina819@users.noreply.github.com>
2019-02-20 08:02:34 -0500

    trabajo de cambio de personaje

--- Assets/Scrips/Player_Controlador.cs
+++ Assets/Scrips/Player_Controlador.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class Player_Controlador : MonoBehaviour
 {
-    private bool hombre;
     
     private Rigidbody2D rgb2;
     private Animator animator;
     private float direccion;
     public static float velocidad = 8;
-    private float posX;
-    private float posY;
     public static float velocidadMax = 10;
    
     // Start is called before the first frame update
@@ -19,26 +16,24 @@ public class Player_Controlador : MonoBehaviour
     {
         rgb2 = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        hombre = animator.GetBool("hombre");
-      
+        
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
         
     }
 
     private void FixedUpdate()
     {
         mermarVelocidadX();
-        verificarGenero();
         RealizarMovimientoCorrer();
     }
     private void RealizarMovimientoCorrer() {
-        posX = rgb2.transform.position.x;
-        posY = rgb2.transform.position.y;
+
 
         if (Input.GetKey(KeyCode.A))
         {
@@ -53,8 +48,7 @@ public class Player_Controlador : MonoBehaviour
             animator.SetFloat("velocidad", Mathf.Abs(rgb2.velocity.x));
 
         }
-            rgb2.transform.position = new Vector3(posX, posY, 0);
-
+      
         if (direccion == -1)
         {
             rgb2.transform.localScale = new Vector3(Mathf.Abs(rgb2.transform.localScale.x), rgb2.transform.localScale.y, rgb2.transform.localScale.z);
@@ -73,27 +67,7 @@ public class Player_Controlador : MonoBehaviour
         }
 
     }
-
- 
-    private void verificarGenero() {
-       
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            animator.SetBool("hombre", !animator.GetBool("hombre"));
-            hombre = !hombre;
-        }
-        if (hombre)
-        {
-          
-            rgb2.transform.localScale = new Vector3(7.2f, 7.2f, 0);
-        }
-        else {
-          
-            rgb2.transform.localScale = new Vector3(0.5f, 0.5f, 0);
-        }
-    }
     private void mermarVelocidadX() {
-
        rgb2.velocity = new Vector3((rgb2.velocity.x * 0.5f), rgb2.velocity.y, 0);
         Debug.Log("entro");
        animator.SetFloat("velocidad", (rgb2.velocity.x * 0.5f));
